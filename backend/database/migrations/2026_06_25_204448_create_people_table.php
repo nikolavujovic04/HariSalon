edb77628cdb4cd2f522e73156f1339a73a8d59a3
Nikola Vujovic <nikolavujovickv@gmail.com>
2026-06-25 22:49:20 +0200

    Kreirani Person i Reservation modeli

--- backend/database/migrations/2026_06_25_204448_create_people_table.php
+++ backend/database/migrations/2026_06_25_204448_create_people_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('people', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('phone_number')->unique();
             $table->timestamps();
         });
     }
