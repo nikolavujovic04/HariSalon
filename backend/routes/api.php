39b7ea1e4487c50a0b5865d0e7e5ceb5c9eda397
Nikola Vujovic <nikolavujovickv@gmail.com>
2026-06-26 17:31:24 +0200

    Implementirane api rute za rezervaciju u api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ReservationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/reservation', [ReservationController::class, 'store']);
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('/reservations', [ReservationController::class, 'index']);
+    Route::get('/reservations/{reservation}',[ReservationController::class, 'show']);
+    Route::put('/reservations/{reservation}', [ReservationController::class, 'update']);
+    Route::delete('/reservations/{reservation}', [ReservationController::class, 'delete']);
+});
+
+Route::login('/login', [AuthController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
