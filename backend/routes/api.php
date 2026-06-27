0bcc710ccf0403dbe3a9a65944d831d45fdaab80
Nikola Vujovic <nikolavujovickv@gmail.com>
2026-06-27 12:26:06 +0200

    Implementirane sve rute u api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,20 +1,24 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HairCutController;
 use App\Http\Controllers\ReservationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/reservations', [ReservationController::class, 'store']);
+Route::get('/haircuts', [HairCutController::class, 'index']);
+Route::get('/haircuts/{hairCut}', [HairCutController::class, 'show']);
 
-Route::post('/reservation', [ReservationController::class, 'store']);
 Route::middleware('auth:sanctum')->group(function(){
     Route::get('/reservations', [ReservationController::class, 'index']);
     Route::get('/reservations/{reservation}',[ReservationController::class, 'show']);
     Route::put('/reservations/{reservation}', [ReservationController::class, 'update']);
     Route::delete('/reservations/{reservation}', [ReservationController::class, 'delete']);
+
+    Route::post('/haircuts', [HairCutController::class, 'store']);
+    Route::put('/haircuts/{hairCut}', [HairCutController::class, 'update']);
+    Route::delete('/haircuts/{hairCut}', [HairCutController::class, 'destroy']);
 });
 
 Route::login('/login', [AuthController::class, 'login']);
