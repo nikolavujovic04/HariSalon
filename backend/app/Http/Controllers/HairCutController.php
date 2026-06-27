710e8c1f01a748a7a1d2f3ceae83c91414490455
Nikola Vujovic <nikolavujovickv@gmail.com>
2026-06-27 12:21:57 +0200

    Kreirane sve operacije u HairCutController-u

--- backend/app/Http/Controllers/HairCutController.php
+++ backend/app/Http/Controllers/HairCutController.php
@@ -53,7 +53,7 @@ class HairCutController extends Controller
      */
     public function show(HairCut $hairCut)
     {
-        //
+        return new HairCutResource($hairCut);
     }
 
     /**
@@ -69,7 +69,15 @@ class HairCutController extends Controller
      */
     public function update(Request $request, HairCut $hairCut)
     {
-        //
+        $request->validate([
+            'type' => 'sometimes|string',
+            'price' => 'sometimes|numeric|min:0',
+            'duration' => 'sometimes|integer|min:1',
+    ]);
+
+        $hairCut->update($request->only(['type', 'price', 'duration']));
+
+        return new HairCutResource($hairCut);
     }
 
     /**
@@ -77,6 +85,10 @@ class HairCutController extends Controller
      */
     public function destroy(HairCut $hairCut)
     {
-        //
+        $hairCut->delete();
+
+        return response()->json(
+            ['message' => 'Deleted succesfully'],201
+        );
     }
 }
